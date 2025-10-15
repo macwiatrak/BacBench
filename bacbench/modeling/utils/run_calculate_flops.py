feat: fix FLOP calculation for GLM2

bacbench/modeling/utils/run_calculate_flops.py
@@ -36,7 +36,7 @@ def calculate_genome_flops(
         seqs = embedder._preprocess_seqs([seq])
         inputs = embedder._tokenize(seqs, max_seq_len=max_seq_len)
         if embedder.model_type in ["glm2", "evo"]:
-            inputs = {"input_ids": inputs["input_ids"]}
+            inputs = {"input_ids": inputs["input_ids"].to(embedder.device)}
         with torch.no_grad():
             fwd_flops, fwd_macs, params = calculate_flops(
                 model=embedder.model,